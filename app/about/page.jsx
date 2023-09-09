c791d0c276d59065300ea0dae9eee36d0da50214
Xavier Zhang <shouxiang.zh@gmail.com>
2023-09-09 21:41:17 +1000

    Initial commit

---
+++ app/about/page.jsx
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: 'About la',
+};
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>From About PAGE </h1>
+      <p>
+        Dummy text here haha Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Vitae consequatur amet animi natus, iure harum laborum explicabo
+        alias beatae accusantium. Non nemo magnam dolorum, asperiores
+        consequuntur itaque. Nesciunt, quasi eos.
+      </p>
+    </div>
+  );
+};
+export default AboutPage;
---
+++ app/about/team/page.jsx
@@ -0,0 +1,14 @@
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit enim,
+        dolorem nisi recusandae mollitia asperiores veritatis ut cum
+        reprehenderit nemo consequatur autem tempore quod sapiente. Eaque sint
+        quidem magnam tempore.
+      </p>
+    </div>
+  );
+};
+export default TeamPage;
