832c4ce8145d8b3327ba38f69f0e91ce3ef5229b
Xavier Zhang <shouxiang.zh@gmail.com>
2023-09-10 18:23:47 +1000

    22

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,33 @@
-import Link from 'next/link'
+'use client';
+import { useState, useEffect } from 'react';
+import LoadingPage from './loading';
+
+import Courses from './components/Courses';
+import CourseSearch from './components/CourseSearch';
 
 const HomePage = () => {
-  return <div>
-    <h1>Welcome to This</h1>
-    <ul>
-      <li><Link href='/'>Home</Link></li>
-      <li><Link href='/about'>About</Link></li>
-      <li><Link href='/about/team'>Team</Link></li>
-    </ul>
-</div>;
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const res = await fetch('/api/courses');
+      const data = await res.json();
+      setCourses(data);
+      setLoading(false);
+    };
+    fetchCourses();
+  }, []);
+
+  if (loading) {
+    return <LoadingPage />;
+  }
+  return (
+    <>
+      <h1>Welcome to This</h1>
+      <CourseSearch getSearchResults={(results)=> setCourses(results)}/>
+      <Courses courses={courses}/>
+    </>
+  );
 };
 export default HomePage;
